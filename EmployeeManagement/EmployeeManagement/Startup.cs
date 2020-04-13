bc3b5bddf7c22caddd979f0a389ba2d0bf19f77b
fkaragoz <fkaragoz@asu.edu>
2020-04-13 00:54:46 -0700

    DefaultFilesOptions to make foo.html as default page

--- EmployeeManagement/EmployeeManagement/Startup.cs
+++ EmployeeManagement/EmployeeManagement/Startup.cs
@@ -33,7 +33,11 @@ namespace EmployeeManagement
             {
                 app.UseDeveloperExceptionPage();
             }
-            app.UseDefaultFiles();
+            DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+            defaultFilesOptions.DefaultFileNames.Clear();
+            defaultFilesOptions.DefaultFileNames.Add("foo.html");
+
+            app.UseDefaultFiles(defaultFilesOptions);
             app.UseStaticFiles();
             
             app.Run(async (context) =>
