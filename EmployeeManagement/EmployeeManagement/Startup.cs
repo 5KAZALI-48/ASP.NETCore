6f1144e72af3e74abc152c6e0226987d484ba9c1
fkaragoz <fkaragoz@asu.edu>
2020-04-13 00:19:37 -0700

    commit

--- EmployeeManagement/EmployeeManagement/Startup.cs
+++ EmployeeManagement/EmployeeManagement/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace EmployeeManagement
 {
@@ -25,7 +26,7 @@ namespace EmployeeManagement
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
         {
             //First Middleware
             if (env.IsDevelopment())
@@ -37,13 +38,22 @@ namespace EmployeeManagement
             //Second Middleware
             app.Use(async (context, next) =>
             {
-                await context.Response.WriteAsync("Hello from 1st middleware\n");
+                logger.LogInformation("MW1: Incoming Request");
                 await next(); //calling also next middleware
+                logger.LogInformation("MW1: Outgoing Response");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW2: Incoming Request");
+                await next(); //calling also next middleware
+                logger.LogInformation("MW2: Outgoing Response");
             });
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello from 2nd middleware");
+                await context.Response.WriteAsync("M3: Request handled and response produced");
+                logger.LogInformation("MW3: Request handled and response produced");
             });
         }
     }
