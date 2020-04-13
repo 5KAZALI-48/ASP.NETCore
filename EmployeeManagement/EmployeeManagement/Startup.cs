fb562b11889f62322ca3bb771f2d730387cabaa5
fkaragoz <fkaragoz@asu.edu>
2020-04-12 21:51:07 -0700

    CreateDefaultBuilder Method calls UseIIS() method and host the application inside IIS worker process (w3wp.exe or issexpress)

---
+++ EmployeeManagement/EmployeeManagement/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmployeeManagement
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //Since we run our project in VS it uses iisexpress in default
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+            });
+        }
+    }
+}
