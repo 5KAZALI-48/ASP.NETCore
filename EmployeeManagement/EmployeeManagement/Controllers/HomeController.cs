ccd72081597c84c3cb6de82c87dda403028594b1
fkaragoz <fkaragoz@asu.edu>
2020-04-13 20:22:09 -0700

    its still dynmaic, we need to make strongly typed

--- EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
@@ -24,10 +24,8 @@ namespace EmployeeManagement.Controllers
         public ViewResult Details()
         {
             Employee model = _employeeRepository.GetEmployee(1);
-            //We use dynmaic properties with the ViewBag
-            ViewBag.Employee = model;
             ViewBag.PageTitle = "Employee Details";
-            return View();
+            return View(model);
         }
     }
 }
