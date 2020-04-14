c0d689373319813461cf4cdfd2db813bd7d4cfa9
fkaragoz <fkaragoz@asu.edu>
2020-04-13 21:17:44 -0700

    Creating an HTML table to display all Employees

--- EmployeeManagement/EmployeeManagement/Models/IEmployeeRepository.cs
+++ EmployeeManagement/EmployeeManagement/Models/IEmployeeRepository.cs
@@ -10,5 +10,6 @@ namespace EmployeeManagement.Models
     public interface IEmployeeRepository
     {
         Employee GetEmployee(int Id);
+        IEnumerable<Employee> GetAllEmployee();
     }
 }
--- EmployeeManagement/EmployeeManagement/Models/MockEmployeeRepository.cs
+++ EmployeeManagement/EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -18,6 +18,12 @@ namespace EmployeeManagement.Models
                 new Employee() { Id = 3, Name = "Sam", Department = "IT", Email = "sam@gmail.com" },
             };
         }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            return _employeeList;
+        }
+
         public Employee GetEmployee(int Id)
         {
             return _employeeList.FirstOrDefault(e => e.Id == Id);
