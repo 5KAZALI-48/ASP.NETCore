9409d11da312e0441fcec8686e7a2f16ef5c0d4c
fkaragoz <fkaragoz@asu.edu>
2020-04-16 01:53:15 -0700

    Adding Photo from Path

--- EmployeeManagement/EmployeeManagement/Models/AppDbContext.cs
+++ EmployeeManagement/EmployeeManagement/Models/AppDbContext.cs
@@ -17,22 +17,7 @@ namespace EmployeeManagement.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Employee>().HasData(
-                new Employee
-                {
-                    Id = 1,
-                    Name = "Mark",
-                    Department = Dept.IT,
-                    Email = "mark@pragimtech.com"
-                },
-                new Employee
-                {
-                    Id = 2,
-                    Name = "John",
-                    Department = Dept.HR,
-                    Email = "john@pragimtech.com"
-                }
-            );
+            modelBuilder.Seed();
         }
     }
 }
--- EmployeeManagement/EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/EmployeeManagement/Models/Employee.cs
@@ -21,5 +21,6 @@ namespace EmployeeManagement.Models
 
         [Required]
         public Dept? Department { get; set; }
+        public string PhotoPath { get; set; }
     }
 }
